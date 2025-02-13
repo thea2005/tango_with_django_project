cba8d87843f98dbe5ee4ec7fec4cb15489b0685e
Thea Salins <2849390s@student.gla.ac.uk>
2025-02-13 10:39:32 +0000

    chapter 9.6

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
+
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -13,5 +15,6 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
 
 # Register your models here.
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
